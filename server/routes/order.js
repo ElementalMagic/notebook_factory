441faa5fbcacbd60f55b08671e516ad7051e2efd
Alex Vahtin <vkstrfrt@gmail.com>
2019-02-23 00:32:59 +0300

    preparing server

--- server/routes/order.js
+++ server/routes/order.js
@@ -6,16 +6,20 @@ var nodemailer = require('nodemailer');
 /* GET home page. */
 router.post('/:type', function (req, res, next) {
     const contactEmail = req.body.email;
-   sendEmail(req, res);
     switch (req.params.type) {
-        case 1: {
-
+        case '1':{
+            sendEmail(req.body, res);
             break;
         }
+        case 'phone':{
+            sendEmailPhone(req.body, res);
+        }
     }
+
+    res.json('Email send');
 });
 
-function sendEmail(req, res){
+function sendEmail(req1, res){
     var transporter = nodemailer.createTransport({
         service: 'gmail',
         auth: {
@@ -23,12 +27,41 @@ function sendEmail(req, res){
             pass: 'pvitkov.ru'
         }
     });
+   let html = req1.body.html;
 
     var mailOptions = {
         from: 'notebook.factory.service@gmail.com',
         to: 'vkstrfrt@gmail.com',
-        subject: '',
-        text: 'That was easy!'
+        subject: 'Заказ расчета с сайта ФабрикаБлокнотов',
+        text: 'Новый заказ!',
+        html: html
+    };
+
+    transporter.sendMail(mailOptions, function (error, info) {
+        if (error) {
+            console.log(error);
+            res.status(400).json(error);
+        } else {
+            console.log('Email sent: ' + info.response);
+            res.status(200).json('Email sent');
+        }
+    });
+}
+function sendEmailPhone(req1, res){
+    var transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+            user: 'notebook.factory.service@gmail.com',
+            pass: 'pvitkov.ru'
+        }
+    });
+    let html = req1.body.html;
+
+    var mailOptions = {
+        from: 'Фабрика Блокнотов',
+        to: 'vkstrfrt@gmail.com',
+        subject: 'Заказ звонка с сайта ФабрикаБлокнотов',
+        html: html
     };
 
     transporter.sendMail(mailOptions, function (error, info) {
