e7c8813127350e1a6f1359e8e99c6574262cbebb
Alex Vahtin <vkstrfrt@gmail.com>
2019-02-24 01:57:24 +0300

    router config

--- server/app.js
+++ server/app.js
@@ -16,6 +16,43 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.get('/products', (req,res) => {
+   res.status(200).sendFile(path.resolve('../../client/fb/products.html'))
+});
+app.get('/bloknot-na-pruzhine', (req,res) => {
+   res.status(200).sendFile(path.resolve('../../client/fb/bloknot-na-pruzhine.html'))
+});
+app.get('/bloknot-na-kleyu', (req,res) => {
+    res.status(200).sendFile(path.resolve('../../client/fb/bloknot-na-kleyu.html'))
+});
+app.get('/bloknot-s-obtyazhkoy', (req,res) => {
+    res.status(200).sendFile(path.resolve('../../client/fb/bloknot-s-obtyazhkoy.html'))
+});
+app.get('/bloknot-s-ruchkoy', (req,res) => {
+    res.status(200).sendFile(path.resolve('../../client/fb/bloknot-s-ruchkoy.html'))
+});
+app.get('/kubariki-s-logotipom', (req,res) => {
+    res.status(200).sendFile(path.resolve('../../client/fb/kubariki-s-logotipom.html'))
+});
+app.get('/bloknot-s-magnitom', (req,res) => {
+    res.status(200).sendFile(path.resolve('../../client/fb/bloknot-s-magnitom.html'))
+});
+app.get('/trebovanya', (req,res) => {
+    res.status(200).sendFile(path.resolve('../../client/fb/trebovanya.html'))
+});
+app.get('/oplata-i-dostavka', (req,res) => {
+    res.status(200).sendFile(path.resolve('../../client/fb/oplata-i-dostavka.html'))
+});
+app.get('/contacts', (req,res) => {
+    res.status(200).sendFile(path.resolve('../../client/fb/contacts.html'))
+});
+app.get('/news', (req,res) => {
+    res.status(200).sendFile(path.resolve('../../client/fb/news.html'))
+});
+app.get('/news-post', (req,res) => {
+    res.status(200).sendFile(path.resolve('../../client/fb/news-post.html'))
+});
+
 app.use(express.static(path.resolve('../../client/fb')));
 //app.use(express.static(path.join('D:','node.js project','FB_PRODUCTION','fb')));
 //app.use(express.static('D:\\node.js project\\FB_PRODUCTION\\fb\\index.html'));
@@ -24,5 +61,5 @@ app.use('/api/order', orderRouter);
 app.use('/users', usersRouter);
 //app.use('*', (req,res) => res.status(400).sendFile('D:\\node.js project\\FB_PRODUCTION\\fb\\index.html'));
 
-app.use('*', (req,res) => res.status(400).sendFile(path.resolve('../../client/fb/index.html')));
+app.use('*', (req,res) => res.status(200).sendFile(path.resolve('../../client/fb/index.html')));
 module.exports = app;
