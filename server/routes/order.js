62c4beb872fb7e9d15a160a96679edeb78ba2c1b
Alex Vahtin <vkstrfrt@gmail.com>
2019-02-23 19:15:44 +0300

    config smtp server

--- server/routes/order.js
+++ server/routes/order.js
@@ -21,17 +21,22 @@ router.post('/:type', function (req, res, next) {
 function sendEmail(req1, res){
     var transporter = nodemailer.createTransport({
         host: "smtp.yandex.ru",
-        port: 587,
-        secure: false,
+        port: 465,
+        secure: true,
+        // dkim:{
+        //     keySelector:'mail',
+        //     domainName: 'fabrikabloknotov.ru',
+        //     privateKey: 'v=DKIM1; k=rsa; t=s; p=MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQDHBPAEu5qfJxymsxcgXg3Bu/JQkq3MRqB8c81VL3bnO/D/UfkWJwR9OE7lGrEnzka07Dl7SveWuJgii1qTYog65O9xAj/cWJ+vvJPo4Mn5PmqKFcvv+mLZciRlMqe8NwE8dbDvTWYekGAmzMT3rIwLH5ERJW90ZE9oeFIw3k8apQIDAQAB'
+        // },
         auth: {
-            user:'informer@fabrikabloknotov.ru',
-            pass: 'nhbnjy89'
+            user:'iqlex1',
+            pass: 've[jvjh'
         }
     });
    let html = req1.body.html;
 
     var mailOptions = {
-        from: 'informer@fabrikabloknotov.ru',
+        from: '"Фабрика блокнотов" <iqlex1@yandex.ru>',
         to: 'vkstrfrt@gmail.com',
         subject: 'Заказ расчета с сайта ФабрикаБлокнотов',
         html: html
@@ -63,7 +68,7 @@ function sendEmailPhone(req1, res){
         }
     });
 
-    let html = '';
+    let html = req1.body.html;
 
     var mailOptions = {
         from: '"Фабрика блокнотов" <iqlex1@yandex.ru>',
